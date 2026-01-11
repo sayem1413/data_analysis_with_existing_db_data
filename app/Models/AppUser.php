37cb12613928974a767992b1f028dc64ad1ac320
Abu Shahadat Md. Sayem <sayem1413@gmail.com>
2026-01-11 13:18:03 +0600

    Start working on splite mobile no with country code

---
+++ app/Models/AppUser.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AppUser extends Model
+{
+    protected $primaryKey = 'userId';
+
+    protected $rememberTokenName = 'passPin';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'userName',
+        'uniqueId',
+        'password',
+        'fullName',
+        'mobileNo',
+        'countryCode',
+        'email',
+        'smsPin',
+        'smsPinStatus',
+        'profileImage',
+        'userType',
+        'passPin',
+        'passPinValidation',
+        'publicKey',
+        'privateKey',
+        'organizationId',
+        'departmentId',
+        'isUnitHead',
+        'imei',
+        'isOrgApproved',
+        'isGpsEnable',
+        'org_office_location_id',
+        'office_employee_id',
+        'employee_reference_id',
+        'email_verified',
+        'mobile_verified',
+        'status',
+        'created_by',
+        'updated_by',
+        'approved_by_admin',
+        'destination_country_id',
+        'reg_stat',
+        'home_my_app_stat',
+        'current_lat',
+        'current_lon',
+        'deleted_at',
+        'deleted_by',
+        'last_used_at',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'passPin',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'password' => 'hashed',
+    ];
+}
